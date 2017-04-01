14017a20f8a8acfe811b2e7c805616574344e80c
Andrew Huynh <a5thuynh@gmail.com>
2017-04-01 12:00:29 -0700

    🎨 whitespace cleanup in planet.js file

--- src/javascript/planet.js
+++ src/javascript/planet.js
@@ -1,161 +1,161 @@
 function Planet( radius ) {
-	this.terrainWidth = 129;
-	this.hMap = [];
-	this.planetRadius = radius;
+    this.terrainWidth = 129;
+    this.hMap = [];
+    this.planetRadius = radius;
 }
 
 Planet.prototype = {
 
-	addVertex: function( verts, v ) {
-		verts.push( v.e( 1 ) );
-		verts.push( v.e( 2 ) );
-		verts.push( v.e( 3 ) );
-	},
-	
-	assignColor: function( h ) {
-		if( h <= 1 ) {
-			return $V( [ 0, 0, 1, 1] ); // Ocean
-		} else if( h <= 4 ) {
-			return $V( [ 0.83, 0.72, 0.34, 1 ] ); // Sand
-		} else if( h <= 10 ) {
-			return $V( [ 0, .6, 0, 1 ] ); // Grass
-		} else {
-			return $V( [ 0.5, 0.5, 0.5, 1 ] ); // Mountains
-		}
-	},
-	
-	generate: function() {
+    addVertex: function( verts, v ) {
+        verts.push( v.e( 1 ) );
+        verts.push( v.e( 2 ) );
+        verts.push( v.e( 3 ) );
+    },
 
-		var planetVertices  = [];
-		var planetIndices   = [];
-		var planetTriangles = [];
-		var planetNormals   = [];
-		var planetColors	= [];
-		
-		var twidth = ( this.terrainWidth - 1 ) * 2.0;
+    assignColor: function( h ) {
+        if( h <= 1 ) {
+            return $V( [ 0, 0, 1, 1] ); // Ocean
+        } else if( h <= 4 ) {
+            return $V( [ 0.83, 0.72, 0.34, 1 ] ); // Sand
+        } else if( h <= 10 ) {
+            return $V( [ 0, .6, 0, 1 ] ); // Grass
+        } else {
+            return $V( [ 0.5, 0.5, 0.5, 1 ] ); // Mountains
+        }
+    },
 
-		// Horizontal points
-		var gammaStep = 2 * Math.PI / twidth;
-		
-		// Vertical points
-		var thetaStep = Math.PI / ( this.terrainWidth - 1 );
+    generate: function() {
 
-		var gamma = 0.0, theta = 0.0;
-		
-		var i, j;
-		
-		var terrain = new ParticleTerrain( this.terrainWidth );
-		this.hMap = terrain.generate( 20, 30, 400, .6 );
+        var planetVertices  = [];
+        var planetIndices   = [];
+        var planetTriangles = [];
+        var planetNormals   = [];
+        var planetColors	= [];
 
-		// Generate vertices
-		for( i = 0; i < twidth; i++ ) {
+        var twidth = ( this.terrainWidth - 1 ) * 2.0;
 
-			gamma = i * gammaStep;
+        // Horizontal points
+        var gammaStep = 2 * Math.PI / twidth;
 
-			for( j = 0; j < this.terrainWidth; j++ ) {
+        // Vertical points
+        var thetaStep = Math.PI / ( this.terrainWidth - 1 );
 
-				theta = j * thetaStep;
+        var gamma = 0.0, theta = 0.0;
 
-				var pt = this.planetPoint( theta, gamma, i, j );
-				planetVertices.push( pt );
-				this.addVertex( planetTriangles, pt );
-				
-				var c = this.assignColor( this.getHeight( i, j ) );
-				
-				planetColors.push( c.e( 1 ) );
-				planetColors.push( c.e( 2 ) );
-				planetColors.push( c.e( 3 ) );
-				planetColors.push( c.e( 4 ) );
-			}
-		} 
+        var i, j;
 
-		// Generate normal
-		for( i = 0; i < twidth; i++ ) {
+        var terrain = new ParticleTerrain( this.terrainWidth );
+        this.hMap = terrain.generate( 20, 30, 400, .6 );
 
-			for( j = 0; j < this.terrainWidth; j++ ) {
+        // Generate vertices
+        for( i = 0; i < twidth; i++ ) {
 
-				var i1 = i * this.terrainWidth + j;
-				var i2 = ( ( i + 1 ) % twidth ) * this.terrainWidth + j;
-				var i3 = ( ( i + 1 ) % twidth ) * this.terrainWidth + j + 1;
-				var i4 = i * this.terrainWidth + j + 1;
+            gamma = i * gammaStep;
 
-				if( j >= this.terrainWidth-1 ) {
-					i3 = this.terrainWidth + j + 1;
-					i4 = j + 1;
-				}
+            for( j = 0; j < this.terrainWidth; j++ ) {
 
-				var v1 = planetVertices[ i1 ];
-				var v2 = planetVertices[ i2 ];
-				var v3 = planetVertices[ i3 ];
-				var v4 = planetVertices[ i4 ];
+                theta = j * thetaStep;
 
-				var normal;
-				var t1, t2, t3, t4;
-				var n1, n2, n3, n4;
+                var pt = this.planetPoint( theta, gamma, i, j );
+                planetVertices.push( pt );
+                this.addVertex( planetTriangles, pt );
 
-				t1 = v1.subtract( v1 );
-				t2 = v2.subtract( v3 );
-				t3 = v3.subtract( v4 );
-				t4 = v4.subtract( v1 );
+                var c = this.assignColor( this.getHeight( i, j ) );
 
-				n1 = t1.cross( t2 ).toUnitVector();
-				n2 = t2.cross( t3 ).toUnitVector();
-				n3 = t3.cross( t4 ).toUnitVector();
-				n4 = t4.cross( t1 ).toUnitVector();
+                planetColors.push( c.e( 1 ) );
+                planetColors.push( c.e( 2 ) );
+                planetColors.push( c.e( 3 ) );
+                planetColors.push( c.e( 4 ) );
+            }
+        }
 
-				normal = n1.add( n2 ).add( n3 ).add( n4 ).toUnitVector();
-				this.addVertex( planetNormals, normal );
-			}
-		}
-		
-		// Generate indices for triangles
-		for( i = 0; i < twidth; i++ ) {
-			for( j = 0; j < this.terrainWidth-1; j++ ) {
+        // Generate normal
+        for( i = 0; i < twidth; i++ ) {
 
-				var i1 = i * this.terrainWidth + j;
-				var i2 = ( ( i + 1 ) % twidth ) * this.terrainWidth + j;
-				var i3 = ( ( i + 1 ) % twidth ) * this.terrainWidth + j + 1;
-				var i4 = i * this.terrainWidth + j + 1;
+            for( j = 0; j < this.terrainWidth; j++ ) {
 
-				planetIndices.push( i1 );
-				planetIndices.push( i2 );
-				planetIndices.push( i3 );
+                var i1 = i * this.terrainWidth + j;
+                var i2 = ( ( i + 1 ) % twidth ) * this.terrainWidth + j;
+                var i3 = ( ( i + 1 ) % twidth ) * this.terrainWidth + j + 1;
+                var i4 = i * this.terrainWidth + j + 1;
 
-				planetIndices.push( i1 );
-				planetIndices.push( i3 );
-				planetIndices.push( i4 );
+                if( j >= this.terrainWidth-1 ) {
+                    i3 = this.terrainWidth + j + 1;
+                    i4 = j + 1;
+                }
 
-			}
-		}
+                var v1 = planetVertices[ i1 ];
+                var v2 = planetVertices[ i2 ];
+                var v3 = planetVertices[ i3 ];
+                var v4 = planetVertices[ i4 ];
 
-		// Free up some memory
-		planetVertices = [];
+                var normal;
+                var t1, t2, t3, t4;
+                var n1, n2, n3, n4;
 
-		return {colors: planetColors, vertices: planetTriangles, 
-					indices: planetIndices, normals: planetNormals };
-	},
-	
-	getHeight: function( i, j ) {
-		var offset;
-		if( i >= this.terrainWidth ) {
-			offset = this.hMap[ ( ( 2 * this.terrainWidth - 1 ) - i ) * this.terrainWidth + j ];
-		} else {
-			offset = this.hMap[ i * this.terrainWidth + j ];
-		}
-		
-		return offset;
-	},
+                t1 = v1.subtract( v1 );
+                t2 = v2.subtract( v3 );
+                t3 = v3.subtract( v4 );
+                t4 = v4.subtract( v1 );
 
-	planetPoint: function( theta, gamma, i, j ) {
-		var offset = this.getHeight( i, j );
-		var pt = this.spherePoint( this.planetRadius, theta, gamma ).toUnitVector();
-		return pt.multiply( this.planetRadius + offset );
-	},
-	
-	spherePoint: function( radius,  theta, gamma ) {
-		var x = radius * Math.sin( theta ) * Math.cos( gamma );
-		var y = radius * Math.cos( theta );
-		var z = radius * Math.sin( theta ) * Math.sin( gamma );
-		return $V([ x, y, z ]);
-	}		
+                n1 = t1.cross( t2 ).toUnitVector();
+                n2 = t2.cross( t3 ).toUnitVector();
+                n3 = t3.cross( t4 ).toUnitVector();
+                n4 = t4.cross( t1 ).toUnitVector();
+
+                normal = n1.add( n2 ).add( n3 ).add( n4 ).toUnitVector();
+                this.addVertex( planetNormals, normal );
+            }
+        }
+
+        // Generate indices for triangles
+        for( i = 0; i < twidth; i++ ) {
+            for( j = 0; j < this.terrainWidth-1; j++ ) {
+
+                var i1 = i * this.terrainWidth + j;
+                var i2 = ( ( i + 1 ) % twidth ) * this.terrainWidth + j;
+                var i3 = ( ( i + 1 ) % twidth ) * this.terrainWidth + j + 1;
+                var i4 = i * this.terrainWidth + j + 1;
+
+                planetIndices.push( i1 );
+                planetIndices.push( i2 );
+                planetIndices.push( i3 );
+
+                planetIndices.push( i1 );
+                planetIndices.push( i3 );
+                planetIndices.push( i4 );
+
+            }
+        }
+
+        // Free up some memory
+        planetVertices = [];
+
+        return {colors: planetColors, vertices: planetTriangles,
+                    indices: planetIndices, normals: planetNormals };
+    },
+
+    getHeight: function( i, j ) {
+        var offset;
+        if( i >= this.terrainWidth ) {
+            offset = this.hMap[ ( ( 2 * this.terrainWidth - 1 ) - i ) * this.terrainWidth + j ];
+        } else {
+            offset = this.hMap[ i * this.terrainWidth + j ];
+        }
+
+        return offset;
+    },
+
+    planetPoint: function( theta, gamma, i, j ) {
+        var offset = this.getHeight( i, j );
+        var pt = this.spherePoint( this.planetRadius, theta, gamma ).toUnitVector();
+        return pt.multiply( this.planetRadius + offset );
+    },
+
+    spherePoint: function( radius,  theta, gamma ) {
+        var x = radius * Math.sin( theta ) * Math.cos( gamma );
+        var y = radius * Math.cos( theta );
+        var z = radius * Math.sin( theta ) * Math.sin( gamma );
+        return $V([ x, y, z ]);
+    }
 };
